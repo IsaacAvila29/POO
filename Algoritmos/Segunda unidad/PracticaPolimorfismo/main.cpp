99ee17e99fb46eb706f1a9fcff5cef2212e19bc8
Isaac Avila <isaac.avila.saenz29@gmail.com>
2024-10-29 10:52:58 -0600

    (con errores) fin de la clase del 29 dde octubre)

--- Algoritmos/Segunda unidad/PracticaPolimorfismo/main.cpp
+++ Algoritmos/Segunda unidad/PracticaPolimorfismo/main.cpp
@@ -6,100 +6,185 @@
 using namespace std;
 
 void mostrarMenu();
-
-bool print(int arreglo[], int tam);
+bool print(int arreglo[], int tam, char *);
 bool print(char *arreglo);
-bool Llenar(int arreglo[], int &tam);                                            //
+bool Llenar(int arreglo[], unsigned int &tam);                                   //
 bool Sumar(int *array1, int tam1, int *array2, int tam2, int *array3, int tam3); //
-char *Sumar(char *cad1, char *cad2)
+char *Sumar(char *cad1, char *cad2);
+
+int main()
 {
-    Conjunto conj1, conj2, conj3; // Declaración de conjuntos
-    unsigned short int cantidad;
+    Conjunto Conj1, Conj2, Conj3;
     typeinfo elDato;
-    int opcion, conjuntoSeleccionado;
-
-    do
+    unsigned int cantidad, cantidad2, cantidad3;
+    cout << "# datos Conj1: ";
+    cin >> cantidad;
+    for (int i = 0; i < cantidad; i++)
     {
-        mostrarMenu(); // Muestra el menú de opciones
-        cout << "Seleccione una opción: ";
-        cin >> opcion;
+        cout << "Teclee un valor entero: ";
+        cin >> elDato;
+        if (Conj1.AddElem(elDato))
+            cout << "Dato insertado!" << endl;
+        else
+            cout << "Error!" << endl;
+    }
+    Conj1.Print();
+    // -----------------------------
+    cout << "# datos Conj2: ";
+    cin >> cantidad;
+    for (int i = 0; i < cantidad; i++)
+    {
+        cout << "Teclee un valor entero: ";
+        cin >> elDato;
+        if (Conj2.AddElem(elDato))
+            cout << "Dato insertado!" << endl;
+        else
+            cout << "Error!" << endl;
+    }
+    Conj2.Print();
 
-        switch (opcion)
+    // la funcion sumar no repite datos
+    if (Sumar(Conj1, Conj2, Conj3))
+    {
+        cout << "***Conjunto 1***" << endl;
+        Conj1.Print();
+        cout << "***Conjunto 2***" << endl;
+        Conj2.Print();
+        cout << "***Conjunto 3***" << endl;
+        Conj3.Print();
+    }
+    else
+        cout << ", error en union!!" << endl;
+
+    ///////////////////////////////////
+
+    cout << "Numero de datos del arreglo 1 ";
+
+    int array1[maxCard], array2[maxCard], array3[maxCard];
+
+    if (Llenar(array1, cantidad))
+        cout << "Datos capturados " << endl;
+    else
+        cout << "Excedidio el limite de datos" << endl;
+
+    if (Llenar(array2, cantidad2))
+        cout << "Datos capturados " << endl;
+    else
+        cout << "Excedidio el limite de datos" << endl;
+    cout << "Hello world!" << endl;
+
+    if (Sumar(array1, cantidad, array2, cantidad2, array3, cantidad3))
+    {
+        cout << "Total de datos generados: " << cantidad2 << endl;
+
+        if (print(array1, cantidad, "Arreglo1"))
         {
-        case 1: // Agregar elementos a un conjunto
-            cout << "Seleccione el conjunto (1 o 2): ";
-            cin >> conjuntoSeleccionado;
-
-            if (conjuntoSeleccionado == 1 || conjuntoSeleccionado == 2)
+            if (print(array2, cantidad2, "Arreglo2"))
             {
-                cout << "¿Cuántos elementos desea agregar?: ";
-                cin >> cantidad;
-
-                for (int i = 0; i < cantidad; i++)
+                if (print(array3, cantidad3, "Arreglo3"))
                 {
-                    cout << "Teclea un valor: ";
-                    cin >> elDato;
-
-                    if ((conjuntoSeleccionado == 1 ? conj1 : conj2).AddElem(elDato))
-                        cout << "Dato insertado correctamente." << endl;
-                    else
-                        cout << "Error: No se pudo insertar el dato." << endl;
                 }
             }
-            else
-            {
-                cout << "Selección inválida." << endl;
-            }
-            break;
-
-        case 2: // Eliminar un elemento de un conjunto
-            cout << "Seleccione el conjunto (1 o 2): ";
-            cin >> conjuntoSeleccionado;
-
-            if (conjuntoSeleccionado == 1 || conjuntoSeleccionado == 2)
-            {
-                cout << "Teclea el valor a eliminar: ";
-                cin >> elDato;
-
-                if ((conjuntoSeleccionado == 1 ? conj1 : conj2).RmvElem(elDato))
-                    cout << "Dato eliminado correctamente." << endl;
-                else
-                    cout << "Error: El dato no se encontró en el conjunto." << endl;
-            }
-            else
-            {
-                cout << "Selección inválida." << endl;
-            }
-            break;
-
-        case 3: // Realizar la unión de los dos conjuntos
-            if (Sumar(conj1, conj2, conj3))
-                cout << "Unión realizada con éxito." << endl;
-            else
-                cout << "Error: No se pudo realizar la unión." << endl;
-
-            cout << "Conjunto resultante de la unión: ";
-            conj3.Print();
-            break;
-
-        case 4: // Imprimir los conjuntos
-            cout << "Conjunto 1: ";
-            conj1.Print();
-            cout << "Conjunto 2: ";
-            conj2.Print();
-            break;
-
-        case 0: // Salir
-            cout << "Saliendo del programa." << endl;
-            break;
-
-        default:
-            cout << "Opción inválida. Intente de nuevo." << endl;
         }
-    } while (opcion != 0); // El menú se repite hasta que se elija la opción 0
+
+        else
+            cout << "Error de impresion" << endl;
+    }
 
     return 0;
 }
+
+// Main con menu
+
+// int main()
+// {
+//     Conjunto conj1, conj2, conj3; // Declaración de conjuntos
+//     unsigned short int cantidad;
+//     typeinfo elDato;
+//     int opcion, conjuntoSeleccionado;
+
+//     do
+//     {
+//         mostrarMenu(); // Muestra el menú de opciones
+//         cout << "Seleccione una opción: ";
+//         cin >> opcion;
+
+//         switch (opcion)
+//         {
+//         case 1: // Agregar elementos a un conjunto
+//             cout << "Seleccione el conjunto (1 o 2): ";
+//             cin >> conjuntoSeleccionado;
+
+//             if (conjuntoSeleccionado == 1 || conjuntoSeleccionado == 2)
+//             {
+//                 cout << "¿Cuántos elementos desea agregar?: ";
+//                 cin >> cantidad;
+
+//                 for (int i = 0; i < cantidad; i++)
+//                 {
+//                     cout << "Teclea un valor: ";
+//                     cin >> elDato;
+
+//                     if ((conjuntoSeleccionado == 1 ? conj1 : conj2).AddElem(elDato))
+//                         cout << "Dato insertado correctamente." << endl;
+//                     else
+//                         cout << "Error: No se pudo insertar el dato." << endl;
+//                 }
+//             }
+//             else
+//             {
+//                 cout << "Selección inválida." << endl;
+//             }
+//             break;
+
+//         case 2: // Eliminar un elemento de un conjunto
+//             cout << "Seleccione el conjunto (1 o 2): ";
+//             cin >> conjuntoSeleccionado;
+
+//             if (conjuntoSeleccionado == 1 || conjuntoSeleccionado == 2)
+//             {
+//                 cout << "Teclea el valor a eliminar: ";
+//                 cin >> elDato;
+
+//                 if ((conjuntoSeleccionado == 1 ? conj1 : conj2).RmvElem(elDato))
+//                     cout << "Dato eliminado correctamente." << endl;
+//                 else
+//                     cout << "Error: El dato no se encontró en el conjunto." << endl;
+//             }
+//             else
+//             {
+//                 cout << "Selección inválida." << endl;
+//             }
+//             break;
+
+//         case 3: // Realizar la unión de los dos conjuntos
+//             if (Sumar(conj1, conj2, conj3))
+//                 cout << "Unión realizada con éxito." << endl;
+//             else
+//                 cout << "Error: No se pudo realizar la unión." << endl;
+
+//             cout << "Conjunto resultante de la unión: ";
+//             conj3.Print();
+//             break;
+
+//         case 4: // Imprimir los conjuntos
+//             cout << "Conjunto 1: ";
+//             conj1.Print();
+//             cout << "Conjunto 2: ";
+//             conj2.Print();
+//             break;
+
+//         case 0: // Salir
+//             cout << "Saliendo del programa." << endl;
+//             break;
+
+//         default:
+//             cout << "Opción inválida. Intente de nuevo." << endl;
+//         }
+//     } while (opcion != 0); // El menú se repite hasta que se elija la opción 0
+
+//     return 0;
+// }
 
 void mostrarMenu()
 {
@@ -130,12 +215,14 @@ bool Llenar(int arreglo[], int &tam)
     return true;
 }
 
-bool print(int arreglo[], int tam)
+bool print(int arreglo[], int tam, char *cadena)
 
 {
     cout << "{ ";
     if (!tam)
         return false;
+
+    cout << cadena;
     for (int i = 0; i < tam; i++)
     {
         cout << arreglo[i] << " ";
@@ -176,7 +263,7 @@ bool Sumar(int *array1, int tam1, int *array2, int tam2, int *array3, int &tam3)
     }
 
     cout << "elementos del arreglo 3" << tam3 << endl;
-    return 0;
+    return true;
 }
 
 char *Sumar(char *cad1, char *cad2)
@@ -184,6 +271,7 @@ char *Sumar(char *cad1, char *cad2)
     int i;
     for (i = 0; *(cad1 + i) != '\0'; i++)
         ;
+
     for (int j = 0; *(cad2 + j) != '\0'; j++)
     {
         if (i == maxCard)
@@ -191,6 +279,7 @@ char *Sumar(char *cad1, char *cad2)
         *(cad1 + i) = *(cad2 + j);
         i++;
     }
+    *(cad1 + i) = '\0';
 
     return cad1;
 }
